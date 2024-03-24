c786446dc4eeec04aab1cb3d6edcdff94dc33245
laraaskovic <lara.askovic@gmail.com>
2024-03-24 18:57:01 -0400

    commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -28,7 +28,9 @@ function App() {
               <BentoBox title="Icon" description="Can:  System.out.println('Hello World');" link="#" image={`${process.env.PUBLIC_URL}/lol.avif`} size="large" />
               <BentoBox title="Library System" description="Java system with GUI Properies. Focused on UI/UX and OOP." link="#" image={`${process.env.PUBLIC_URL}/r.avif`} size="medium" />
               <BentoBox title="Chat Application" description="Something else." link="#" image={`${process.env.PUBLIC_URL}/ll.jpg`} size="small" />
-              {/* Add more BentoBoxes as needed */}
+              <BentoBox title="Icon" description="Can:  System.out.println('Hello World');" link="#" image={`${process.env.PUBLIC_URL}/lol.avif`} size="large" />
+              <BentoBox title="Chat Application" description="Something else." link="#" image={`${process.env.PUBLIC_URL}/ll.jpg`} size="small" />
+            {/* Add more BentoBoxes as needed */}
              </div>
 
 
