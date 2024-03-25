780603907da56a01ab9f41ff470f88960dec0dff
laraaskovic <lara.askovic@gmail.com>
2024-03-25 09:27:13 -0400

    commit

--- portfolio/src/components/BentoBox/BentoBox.js
+++ portfolio/src/components/BentoBox/BentoBox.js
@@ -13,3 +13,21 @@ const BentoBox = ({ title, description, link, image, size }) => (
 );
 
 export default BentoBox;
+
+/*import React from 'react';
+import './BentoBox.css'; 
+
+const BentoBox = ({ title, description, link, image, size }) => (
+  <div className={`bento-box ${size}`}>
+    <img src={image} alt={title} className="bento-image" />
+    <div className="content">
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <a href={link}>Learn More</a>
+    </div>
+  </div>
+);
+
+export default BentoBox;
+
+*/
